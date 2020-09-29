8fc0ee8e4c805087f29e173d451c0e60162eb34c
Casper Mogensen <casper.mogensen@gmail.com>
2020-09-29 10:26:30 +0200

    Add print_warning

--- src/debug.h
+++ src/debug.h
@@ -6,6 +6,7 @@
 
 #define print_fatal(...) { fprintf(stderr, "FATAL from %s: Line: %d: ",__FILE__,__LINE__); fprintf(stderr,__VA_ARGS__); fflush(stderr); exit(EXIT_FAILURE);}
 #define print_error(...) { fprintf(stderr, "ERROR from %s: Line: %d: ",__FILE__,__LINE__); fprintf(stderr,__VA_ARGS__); fflush(stderr); }
+#define print_warning(...) { fprintf(stderr, "WARNING from %s: Line: %d: ",__FILE__,__LINE__); fprintf(stderr,__VA_ARGS__); fflush(stderr); }
 #define print_info(...) { fprintf(stderr,  "INFO  from %s: Line: %d: ",__FILE__,__LINE__); fprintf(stderr,__VA_ARGS__); }
 
 
